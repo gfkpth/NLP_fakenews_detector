da3a6064ab3fa457da51d1a8d537c3dea64e6037
Simbs-art <musasimbiat@gmail.com>
2025-06-18 11:18:26 +0100

    visualization

--- 01-dataexplore.py
+++ 01-dataexplore.py
@@ -74,6 +74,48 @@ print('Test for missing values:', annotated.isnull().sum())
 
 # none, very good
 
+# %%
+# Add a column for text length
+annotated['text_length'] = annotated['text'].apply(len)
+
+
+# Describe text lengths
+print(annotated['text_length'].describe())
+
+# Plot distribution
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.figure(figsize=(8, 5))
+sns.histplot(annotated['text_length'], bins=30, kde=True)
+plt.title("Distribution of Headline Lengths")
+plt.xlabel("Number of Characters")
+plt.ylabel("Frequency")
+plt.show()
+# %%
+# World cloud for real and fake news
+from wordcloud import WordCloud
+
+# Seperate headlines by label
+real_news = annotated[annotated['label'] == '1']['text']
+fake_news = annotated[annotated['label'] == '0']['text']
+
+# Plot word cloud for real news
+plt.figure(figsize=(12, 6))
+wordcloud_real = WordCloud(width=800, height=400, background_color='white').generate(' '.join(real_news))
+plt.imshow(wordcloud_real, interpolation='bilinear')
+plt.axis('off')
+plt.title("Word Cloud for Real News")
+plt.show()
+# %%
+# Plot word cloud for fake news
+plt.figure(figsize=(12, 6))
+wordcloud_fake = WordCloud(width=800, height=400, background_color='white').generate(' '.join(fake_news))
+plt.imshow(wordcloud_fake, interpolation='bilinear')
+plt.axis('off')
+plt.title("Word Cloud for Fake News")
+plt.show()
+# %%
 
 # Let's search for special characters we need to take care of
 
@@ -112,3 +154,5 @@ punctnohyphen
 # bi_tokens = list(bigrams(tokens))
 # print(tokens)
 # print(bi_tokens)
+
+# %%
